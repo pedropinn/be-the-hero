e8dbb3a3a21c4d8ef96e723bb55fa9a4500aa86b
pedropinn <pedropinn@gmail.com>
2020-03-26 23:34:54 -0300

    frontend web, add readme

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+
+    cd backend/
+    yarn install 
+    yarn start
+
+
+    cd frontend/
+    yarn install
+    yarn start
+    
---
+++ frontend/src/services/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const api = axios.create({ 
+    baseURL: 'http://localhost:3333',
+})
+
+//d69f78a1
+//82a07d2d
+export default api;
